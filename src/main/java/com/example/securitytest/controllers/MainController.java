890d54ddb758425a7f5bdf33d04465910bc2773c
ehan69rus <ehan1996@mail.ru>
2023-02-28 22:17:44 +0300

    First commit

---
+++ src/main/java/com/example/securitytest/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.example.securitytest.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String homePage() {
+        return "hello";
+    }
+}
